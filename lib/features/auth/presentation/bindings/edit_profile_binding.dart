a6d29c4bed72ea95938534ce35bab0207507944f
Rushi <rushi.creates@gmail.com>
2025-04-11 15:24:55 +0530

    bindings and named routes added for few screens

--- lib/features/auth/presentation/bindings/edit_profile_binding.dart
+++ lib/features/auth/presentation/bindings/edit_profile_binding.dart
@@ -1,9 +1,9 @@
-import 'package:auth_app1/features/auth/presentation/controllers/profile_controller.dart';
-import 'package:get/get.dart';
+// import 'package:auth_app1/features/auth/presentation/controllers/profile_controller.dart';
+// import 'package:get/get.dart';
 
-class ProfileBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => ProfileController());
-  }
-}
+// class EditProfileBinding extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut(() => EditProfileController());
+//   }
+// }
